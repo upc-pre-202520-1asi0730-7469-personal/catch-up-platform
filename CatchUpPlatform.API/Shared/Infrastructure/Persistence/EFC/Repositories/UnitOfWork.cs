8f50414b7271e8ee41ae81f96f15b13f08f9c525
Gerardo Palacin <gerardopalacin.github@gmail.com>
2025-10-21 22:37:50 -0500

    feat(repositories): add base repository and unit of work implementations for data access.

---
+++ CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using CatchUpPlatform.API.Shared.Domain.Repositories;
+using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+public class UnitOfWork (AppDbContext context): IUnitOfWork
+{
+
+    public async Task CompleteAsync()
+    {
+        await context.SaveChangesAsync();
+    }
+    
+    
+    
+}
