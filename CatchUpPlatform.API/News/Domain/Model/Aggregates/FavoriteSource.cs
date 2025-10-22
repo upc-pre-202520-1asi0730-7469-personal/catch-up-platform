cf1063f2f9b0fe165147a9d2ca8693ab4e48918b
Gerardo Palacin <gerardopalacin.github@gmail.com>
2025-10-21 22:56:13 -0500

    feat(news): implement favorite source aggregate and create favorite source command for managing favorite news sources.

---
+++ CatchUpPlatform.API/News/Domain/Model/Aggregates/FavoriteSource.cs
@@ -0,0 +1,30 @@
+﻿using CatchUpPlatform.API.News.Domain.Model.Commands;
+
+namespace CatchUpPlatform.API.News.Domain.Model.Aggregates;
+
+public partial class FavoriteSource
+{
+    
+    public int Id { get; }
+    
+    public string NewsApiKey { get; private set; }
+    public string SourceId { get; private set; }
+
+    protected FavoriteSource()
+    {
+        NewsApiKey = string.Empty;
+        SourceId = string.Empty; 
+    }
+    
+    public FavoriteSource(string newsApiKey, string sourceId)
+    {
+        NewsApiKey = newsApiKey;
+        SourceId = sourceId;
+    }
+    
+    public FavoriteSource(CreateFavoriteSourceCommand command)
+    {
+        NewsApiKey = command.NewsApiKey;
+        SourceId = command.SourceId;
+    }
+}
---
+++ CatchUpPlatform.API/News/Domain/Model/Commands/CreateFavoriteSourceCommand.cs
@@ -0,0 +1,4 @@
+﻿namespace CatchUpPlatform.API.News.Domain.Model.Commands;
+
+public record CreateFavoriteSourceCommand(string NewsApiKey, string SourceId);
+
