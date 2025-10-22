d066146c9e6c87cb7e8eda364776019e55dfd525
Gerardo Palacin <gerardopalacin.github@gmail.com>
2025-10-21 23:23:42 -0500

    feat(favorite-source): add command and query service contracts for managing favorite sources.

---
+++ CatchUpPlatform.API/News/Domain/Services/IFavoriteSourceCommandService.cs
@@ -0,0 +1,10 @@
+﻿using CatchUpPlatform.API.News.Domain.Model.Aggregates;
+using CatchUpPlatform.API.News.Domain.Model.Commands;
+
+namespace CatchUpPlatform.API.News.Domain.Services;
+
+public interface IFavoriteSourceCommandService
+{
+    Task<FavoriteSource?> Handle(CreateFavoriteSourceCommand command);
+    
+}
